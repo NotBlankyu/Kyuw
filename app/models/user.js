da6b5ad9080a8e55ded7a9d6ead2665d6503b921
NotBlankyu <66680364+NotBlankyu@users.noreply.github.com>
2020-06-09 14:00:37 +0100

    Add files via upload

---
+++ app/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const guildSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    userID: String, 
+    gold: Number, default: 0,
+    dailytime: Number
+    
+  
+    
+});
+
+module.exports = mongoose.model('User', guildSchema, 'users');
