b625a685a3ce6aa76aa1807263ed43e0c1247fca
dragonhuntermlw <dragonhuntermlw@gmail.com>
2020-06-21 19:03:38 +0000

    added permissions list to userinfo

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -12,18 +12,18 @@ module.exports = {
   
   run : async (client, message, args) => {
 
-  let member = message.mentions.users.first()|| message.guild.members.cache.get(member => member.id == args[1]) || message.member;
+  let member = message.guild.member(message.mentions.members.first())|| message.guild.members.cache.find(member => member.id == args[0]) || message.member;
   let Embed = new Discord.MessageEmbed()
 	.setColor('#0099ff')
-	.setTitle(member.user.username)
+	.setTitle( member.user.username)
 	.setAuthor(message.author.username, message.author.avatarURL())
 	.setDescription('User Info')
 	.setThumbnail(member.user.avatarURL())
 	.addFields(
 		{ name: 'User Id', value: member.id, inline: false },
-    { name: 'Created At', value: member.user.createdAt, inline: false },
+    { name: 'Created At', value:moment(member.user.createdAt).locale('en-gb').format('LLL'), inline: false },
     { name: 'Joined At', value:moment(member.joinedAt).locale('en-gb').format('LLL'), inline: false },
-    { name: 'Permissions', value:moment(member.joinedAt).locale('en-gb').format('LLL'), inline: false },
+    { name: 'Permissions', value:member.permissions.toArray(), inline: false },
     
 	)
 	.setTimestamp()
